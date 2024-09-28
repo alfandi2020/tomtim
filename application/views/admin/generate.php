fc8b0bceb4042dc560dd84f584031eca1bff7f31
alfandi2020 <pandibl4530@gmail.com>
2024-09-28 22:29:00 +0700

    a

--- application/views/admin/generate.php
+++ application/views/admin/generate.php
@@ -42,139 +42,139 @@
             </div>
                                <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="A">Interface A:</label>
+        <b for="A">Interface A:</b>
         <input type="text" id="A" class="form-control border border-primary" placeholder="Enter Interface A (e.g. gpon-olt_1/1/7)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="B">ONU Number B:</label>
+        <b for="B">ONU Number B:</b>
         <input type="text" id="B" class="form-control border border-primary" placeholder="Enter ONU Number B (e.g. 13)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="C">Serial Number C:</label>
+        <b for="C">Serial Number C:</b>
         <input type="text" id="C" class="form-control border border-primary" placeholder="Enter Serial Number C (e.g. ZTEGCC599CD3)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="D">Interface D:</label>
+        <b for="D">Interface D:</b>
         <input type="text" id="D" class="form-control border border-primary" placeholder="Enter Interface D (e.g. gpon-onu_1/1/7:13)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="E">Name E:</label>
+        <b for="E">Name E:</b>
         <input type="text" id="E" class="form-control border border-primary" placeholder="Enter Name E (e.g. DHARMA AJI NUGRAHA)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="F">Description F:</label>
+        <b for="F">Description F:</b>
         <input type="text" id="F" class="form-control border border-primary" placeholder="Enter Description F (e.g. 13$$DHARMA AJI NUGRAHA$$)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="G">Profile G:</label>
+        <b for="G">Profile G:</b>
         <input type="text" id="G" class="form-control border border-primary" placeholder="Enter Profile G (e.g. 60M)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="H">Profile H:</label>
+        <b for="H">Profile H:</b>
         <input type="text" id="H" class="form-control border border-primary" placeholder="Enter Profile H (e.g. 100M)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="I">Profile I (TR069):</label>
+        <b for="I">Profile I (TR069):</b>
         <input type="text" id="I" class="form-control border border-primary" placeholder="Enter Profile I (e.g. TR069)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="J">Upstream J (T1):</label>
+        <b for="J">Upstream J (T1):</b>
         <input type="text" id="J" class="form-control border border-primary" placeholder="Enter Upstream J (e.g. 60M)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="K">Downstream K (T1):</label>
+        <b for="K">Downstream K (T1):</b>
         <input type="text" id="K" class="form-control border border-primary" placeholder="Enter Downstream K (e.g. 60M)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="L">Upstream L (T2):</label>
+        <b for="L">Upstream L (T2):</b>
         <input type="text" id="L" class="form-control border border-primary" placeholder="Enter Upstream L (e.g. 100M)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="M">Downstream M (T2):</label>
+        <b for="M">Downstream M (T2):</b>
         <input type="text" id="M" class="form-control border border-primary" placeholder="Enter Downstream M (e.g. 100M)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="N">Upstream N (TR069):</label>
+        <b for="N">Upstream N (TR069):</b>
         <input type="text" id="N" class="form-control border border-primary" placeholder="Enter Upstream N (e.g. 2M)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="O">Downstream O (TR069):</label>
+        <b for="O">Downstream O (TR069):</b>
         <input type="text" id="O" class="form-control border border-primary" placeholder="Enter Downstream O (e.g. 2M)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="P">User VLAN P (INTERNET):</label>
+        <b for="P">User VLAN P (INTERNET):</b>
         <input type="text" id="P" class="form-control border border-primary" placeholder="Enter User VLAN P (e.g. 120)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="Q">VLAN Q (INTERNET):</label>
+        <b for="Q">VLAN Q (INTERNET):</b>
         <input type="text" id="Q" class="form-control border border-primary" placeholder="Enter VLAN Q (e.g. 120)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="R">User VLAN R (HOTSPOT):</label>
+        <b for="R">User VLAN R (HOTSPOT):</b>
         <input type="text" id="R" class="form-control border border-primary" placeholder="Enter User VLAN R (e.g. 10)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="S">VLAN S (HOTSPOT):</label>
+        <b for="S">VLAN S (HOTSPOT):</b>
         <input type="text" id="S" class="form-control border border-primary" placeholder="Enter VLAN S (e.g. 10)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="T">User VLAN T (TR69):</label>
+        <b for="T">User VLAN T (TR69):</b>
         <input type="text" id="T" class="form-control border border-primary" placeholder="Enter User VLAN T (e.g. 100)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="U">VLAN U (TR69):</label>
+        <b for="U">VLAN U (TR69):</b>
         <input type="text" id="U" class="form-control border border-primary" placeholder="Enter VLAN U (e.g. 100)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="V">PON-ONU-MNG Interface V:</label>
+        <b for="V">PON-ONU-MNG Interface V:</b>
         <input type="text" id="V" class="form-control border border-primary" placeholder="Enter Interface V (e.g. gpon-onu_1/1/7:13)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="W">VLAN W (INTERNET Service):</label>
+        <b for="W">VLAN W (INTERNET Service):</b>
         <input type="text" id="W" class="form-control border border-primary" placeholder="Enter VLAN W (e.g. 120)">
       </div>
       <div class="col-md-6 mt-4">
-        <label for="X">VLAN X (HOTSPOT Service):</label>
+        <b for="X">VLAN X (HOTSPOT Service):</b>
         <input type="text" id="X" class="form-control border border-primary" placeholder="Enter VLAN X (e.g. 10)">
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-6 mt-4">
-        <label for="Y">VLAN Y (TR069 Service):</label>
+        <b for="Y">VLAN Y (TR069 Service):</b>
         <input type="text" id="Y" class="form-control border border-primary" placeholder="Enter VLAN Y (e.g. 100)">
       </div>
     </div>
