1b1d59e3b588d9ebcdbac79b7d360ad5fc6fb320
alfandi2020 <pandibl4530@gmail.com>
2025-05-11 11:42:07 +0700

    a

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -285,7 +285,7 @@ class Cron extends CI_Controller
                                 ));
                                 $response = curl_exec($curl);
                                 curl_close($curl);
-                                echo $response;
+                                echo $response . $x->nama;
                             }
                         // }
                     // }
